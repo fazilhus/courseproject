342a4dc77a9db298b7574de33aa073c83a196bfb
фазиль хуснутдинов <fikhusnutdinov@miem.hse.ru>
2021-03-29 04:53:14 +0300

    Fixed bugs

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 29 04:02:39 2021
+
+@author: khusn
+"""
+
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
@@ -16,9 +23,9 @@ def encode_lz78(s):
     progress_bar['maximum'] = len(s)
     time = t.time()
     for i in range(len(s)):
-        if t.time() - time > 0.03:
+        if t.time() - time > 0.05:
             time = t.time()
-            progress_bar['value'] = i
+            progress_bar['value'] += i
             root.update()
         if (buffer + s[i]) in dictionary.keys():
             buffer += s[i]
@@ -41,25 +48,26 @@ def encode_lz78(s):
 
 # Функция отвечающая за непосредственно разархивирование выбранного файла
 def decode_lz78(inp):
+    s = inp[1:-1].split(')(')
     encoded = []
-    for i in range(inp.find('|'), len(inp)):
-        if inp[i] == '|' and inp[i - 1] != ',':
-            encoded.append(inp[i - 3:i].split(','))
-    encoded.append(inp[inp.rfind('|') + 1:].split(','))
-    dictionary = ['']
+    for i in range(len(s)):
+        pos = s[i].find(',')
+        encoded.append([int(s[i][:pos]), s[i][pos + 1:]])
+    lst = ['']
     ans = ''
     progress_bar['maximum'] = len(encoded)
     time = t.time()
     for i in range(len(encoded)):
-        if t.time() - time > 0.03:
+        if t.time() - time > 0.05:
             time = t.time()
-            progress_bar['value'] = i
+            progress_bar['value'] += i
             root.update()
         node = encoded[i]
-        word = dictionary[int(node[0])] + node[1]
+        word = lst[node[0]] + node[1]
         ans += word
-        dictionary.append(word)
+        lst.append(word)
     return ans
+
 
 # Функция, вызываемя при срабатывании кнопки "Открыть". Сохраняет путь к файлу в глобальной переменной.
 def open_file():
@@ -84,16 +92,17 @@ def encode():
 
     with open(file_path, 'rb') as f:
         encoding = cchardet.detect(f.read())['encoding']
-        print(encoding)
 
-    with open(file_path, 'r', encoding='utf-8') as f:
-        ans1 = encode_lz78(f.read())
+    f = open(file_path, 'r', encoding=encoding)
+    s = f.read()
+    ans1 = encode_lz78(s)
+    f.close()
 
-    file_path = os.path.splitext(file_path)[0] + '.lz78'
-    with open(file_path, 'w', encoding='utf-8') as f:
-        for elem in ans1[:-1]:
-            f.write(str(elem[0]) + ',' + elem[1] + '|')
-        f.write(str(ans1[-1][0]) + ',' + ans1[-1][1])
+    file_path = file_path[:-4] + '.lz78'
+    f = open(file_path, 'w', encoding=encoding)
+    for i in range(len(ans1)):
+        f.write('(' + str(ans1[i][0]) + ',' + ans1[i][1] + ')')
+    f.close()
 
     path_result = tk.StringVar(root)
     file_path_label['textvariable'] = path_result
@@ -106,16 +115,17 @@ def encode():
 def decode():
     global file_path
 
-    '''with open(file_path, 'rb') as f:
+    with open(file_path, 'rb') as f:
         encoding = cchardet.detect(f.read())['encoding']
-        print(encoding)'''
 
-    with open(file_path, 'r', encoding='utf-8') as f:
-        ans2 = decode_lz78(f.read())
+    f = open(file_path, 'r', encoding=encoding)
+    ans2 = decode_lz78(f.read())
+    f.close()
 
-    file_path = os.path.splitext(file_path)[0] + '.txt'
-    with open(file_path, 'w', encoding='utf-8') as f:
-        f.write(ans2)
+    file_path = file_path[:-5] + '.txt'
+    f = open(file_path, 'w', encoding=encoding)
+    f.write(ans2)
+    f.close()
 
     path_result = tk.StringVar(root)
     file_path_label['textvariable'] = path_result
@@ -123,6 +133,8 @@ def decode():
 
     dearchive['state'] = 'disabled'
     progress_bar['value'] = 0.0
+
+
 
 # Инициализация окна приложения
 root = tk.Tk()
